d416774d933d4b089c290e313300e72ddef6ea7b
mazab99 <mazab15499@gmail.com>
2023-02-22 09:52:59 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'Nothing/ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      useInheritedMediaQuery: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: NothingExample(),
+    );
+  }
+}
+
+
